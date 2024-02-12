28069b319d3ea9912a423e6912051365c4654748
Vlada Hudii <vlada.hudii@laba.com>
2024-02-12 16:23:06 +0200

    yes

--- dist/index.579125c3.js
+++ dist/index.579125c3.js
@@ -1,2 +1,18 @@
+document.addEventListener("DOMContentLoaded", function() {
+    window.onscroll = function() {
+        scrollFunction();
+    };
+    function scrollFunction() {
+        if (window.scrollY > 100) document.getElementById("scrollTopButton").style.display = "block";
+        else document.getElementById("scrollTopButton").style.display = "none";
+    }
+    function scrollTopFunction() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+    document.getElementById("scrollTopButton").addEventListener("click", scrollTopFunction);
+});
 
 //# sourceMappingURL=index.579125c3.js.map
--- src/index.js
+++ src/index.js
@@ -1,2 +1,19 @@
+document.addEventListener('DOMContentLoaded', function(){
+    
+    window.onscroll = function() {
+        scrollFunction();
+    }
+    function scrollFunction () {
+        if ( window.scrollY > 100 ) {
+            document.getElementById( 'scrollTopButton').style.display = 'block'
+        } else {
+            document.getElementById( 'scrollTopButton').style.display = 'none'
+        }
+    }
 
+    function scrollTopFunction() {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
 
+    document.getElementById('scrollTopButton').addEventListener('click', scrollTopFunction)
+})
