2ad7214963e43d3cd6bd6ea21eb47c057846187d
Vlada Hudii <vlada.hudii@laba.com>
2024-02-28 18:12:10 +0200

    +

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-
+document.addEventListener('DOMContentLoaded', function(){
+    // code
+    const buttonMenu = document.querySelector('.mobileMenuButton');
+    buttonMenu.addEventListener('click', function () {
+        document.querySelector('.navigation').classList.toggle('visible');
+        buttonMenu.classList.toggle('open');
+    });
+})
 
